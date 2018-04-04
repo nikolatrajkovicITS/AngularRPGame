c9a91c5a2d6978ae6c8e833d95121011f72f3a2f
FUSION\nikola.trajkovic <nikola.trajkovic@bluecouchdigital.com>
2018-04-04 16:45:37 +0200

    Created a structure of project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app.component';
+import { CharacterCreationComponent } from './components/character-creation/character-creation.component';
+import { FightComponent } from './components/fight/fight.component';
+import { InventoryComponent } from './components/inventory/inventory.component';
+import { StartComponent } from './components/start/start.component';
+import { StoryComponent } from './components/story/story.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CharacterCreationComponent,
+    FightComponent,
+    InventoryComponent,
+    StartComponent,
+    StoryComponent
   ],
   imports: [
     BrowserModule
