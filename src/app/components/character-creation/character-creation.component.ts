9ae59d314fd0dae9ad33fff9e2179c002c49e722
FUSION\nikola.trajkovic <nikola.trajkovic@bluecouchdigital.com>
2018-04-09 19:16:01 +0200

    Implemented creating character functionality

--- src/app/components/character-creation/character-creation.component.ts
+++ src/app/components/character-creation/character-creation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CharacterOptions } from '../../models/character-options';
 
 @Component({
   selector: 'app-character-creation',
@@ -6,10 +7,57 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./character-creation.component.css']
 })
 export class CharacterCreationComponent implements OnInit {
+  character = {
+    race: '--Choose--',
+    class: '--Choose--',
+    gender: undefined,
+    name: undefined
+  }
+
+  races = CharacterOptions.races;
+  classes = CharacterOptions.classes;
+  genders = CharacterOptions.genders;
+  
+  characterComplete: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  changeRace(race: string) {
+    this.character.race = race;
+    this.checkCompleted();
+  }
+
+  changeClass(newClass: string) {
+    this.character.class = newClass;
+    this.checkCompleted();    
+  }
+
+  changeGender(gender: string) {
+    this.character.gender = gender;
+    this.checkCompleted();        
+  }
+
+  changeName() {
+    this.checkCompleted();        
+  }
+
+  checkCompleted() {
+    this.characterComplete = this.character.race !== "--Choose--"
+      && this.character.class !== "--Choose--"
+      && this.character.gender
+      && this.character.name
+  }
+
+  createCharacter() {
+    if (!this.characterComplete) {
+      return;
+    }
+
+    console.log(this.character);
+  }
+
 }
+
