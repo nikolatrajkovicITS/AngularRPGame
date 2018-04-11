ecdd4b85803c415e6e916f7bc3c9895d6ed83514
FUSION\nikola.trajkovic <nikola.trajkovic@bluecouchdigital.com>
2018-04-11 16:23:48 +0200

    Created Character class

--- src/app/models/characters.ts
+++ src/app/models/characters.ts
@@ -29,7 +29,7 @@ export enum FightOptions {
     none = "None"       
 }
 
-export const ExperianceToLevel = {
+export const ExperienceToLevel = {
     1: 1000,
     2: 2000,
     3: 3000,
@@ -46,8 +46,8 @@ export class BaseCharacter {
     maxHealth: number;
     currentHealth: number;
     /**
-     * If a character is incapacitated they wont be able to interact
-     * or be involved in the fight, enemies wont target them and 
+     * If a character is incapacitated they won't be able to interact
+     * or be involved in the fight, enemies won't target them and 
      * they won't be able to have a turn.
      */
     isIncapacitated: boolean;     
@@ -141,5 +141,96 @@ export class Monster extends BaseCharacter {
          * Image for our monster
          */
         this.spriteUrl = spriteUrl;
+    }
+}
+
+export class Hero extends BaseCharacter { 
+    gender: string;
+    race: string;
+    characterRole: string;
+    experience: number;
+    level: number;
+    /**
+     * When our hero's level up they'll
+     * gain some available skill points
+     * to upgrade their various skills.
+     */
+    availableSkillPoints: number;
+    /**
+     * When the Ranger uses his special attack 
+     * he can select an ally to defend with a
+     * trap. (the next time when monsters try 
+     * to attack the character, they will be 
+     * stopped and lose their next turn)
+     */
+    hasTrapDefence: boolean;
+    /**
+     * Once the Ranger levels up his trap not
+     * only takes their turn but deals damage
+     * as well.
+     */
+    hasDamagingTrap: boolean;
+    /**
+     * You can use only your special attack
+     * the way I will set it, every outher turn.
+     * So we need to keep track of, if they've
+     * used the recently how long they have 
+     * until they can use it again.
+     */
+    turnsUntilSpecialAvailableAgain: number;
+
+    constructor(name, gender, race, level, health, skills, weapon, armor) {
+        super(name, health, skills);
+
+        this.gender = gender;
+        this.race = race;
+        this.experience = 0;
+        this.level = level;
+        this.equippedWeapon = weapon;
+        this.equippedArmor(armor);
+    }
+
+    levelUp(): void {
+        this.experience -= ExperienceToLevel[this.level];
+        this.level++;
+        this.availableSkillPoints +=2;
+        /**
+         * If user end up being a boss and getting a crazy amount
+         * of experience you can level up multiple times in a row
+         * without having to run a weird number of checks in the 
+         * code in game controller.
+         */
+        if (this.experience >= ExperienceToLevel[this.level]) {
+            this.levelUp();
+        }
+    }
+
+    /**
+     * 
+     */
+    equipNewArmor(armor: Armor): void {
+        /**
+         * If they do have some equipt armor then we want to reduce
+         * our attack-barrier bonus by whatever that armor was giving
+         * us. 
+         */
+        if (this.equippedArmor) {
+            this.barriers.attack -= this.equippedArmor.attackBarrierBonus;
+        }
+        this.equippedArmor = armor;
+        this.barriers.attack += armor.attackBarrierBonus;
+    }
+    
+    equipNewWeapon(weapon: Weapon): void {
+        this.equippedWeapon = weapon;
+    }
+
+    /**
+     * For healing our character
+     */
+    rest(): void {
+       this.currentHealth = this.maxHealth;
+       this.isIncapacitated = false;
+       this.turnsUntilSpecialAvailableAgain = 0;
     }
 }
