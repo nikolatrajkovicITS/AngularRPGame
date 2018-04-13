c72316b6ee9e4174a8ec5b54896b6e2dd9dd078e
FUSION\nikola.trajkovic <nikola.trajkovic@bluecouchdigital.com>
2018-04-13 12:47:36 +0200

    Created chapter two

--- src/app/chapters/chapter-two.ts
+++ src/app/chapters/chapter-two.ts
@@ -8,9 +8,8 @@ import { Warrior } from '../models/hero/warrior';
 import { GenderOptions } from '../models/character/gender-options';
 import { RaceOptions } from '../models/character/race-options';
 import { Armor } from "../models/armor";
-import { Chapter2 } from './chapter-two';
 
-export const Chapter1: Chapter = {
+export const Chapter2: Chapter = {
   story: [
     `You enter the woods, chasing after the goblin who stole your father's sword. You lose sight of them in the thick woods and begin to creep forward, relyingon your ears to warm you of danger and hopefully to locate the theiving goblin.`,
     `Shenanigans ensue and an encounter begins. Now the question is: How do you want to handle it?`
@@ -18,9 +17,10 @@ export const Chapter1: Chapter = {
   options: [
     CharacterAction.attack,
     CharacterAction.sneak,
-    CharacterAction.persuade
+    CharacterAction.persuade    
   ],
   enemyParty: [
+    new Monster("Orc", 7, {attack: 4, sneak: 0, persuade: 0}, {attack: 11, sneak: 11, persuade: 11}, 2, 5, "../../assets/images/orc.png"),
     new Monster("Goblin", 5, {attack: 2, sneak: 0, persuade: 0}, {attack: 10, sneak: 10, persuade: 10}, 1, 3, "../../assets/images/goblin.png")
   ],
   sneakPersuadeFail: CharacterAction.attack,
@@ -31,9 +31,9 @@ export const Chapter1: Chapter = {
     SuccessOptions.addHeroToParty
   ],
   rewards: {
-    experience: 500,
-    equipment: [new Weapon("Rusty Sword", 1, 6)],
-    newHero: new Warrior("Donderion", GenderOptions.male, RaceOptions.human, 1, 7, {attack: 3, sneak: 2, persuade: 1, intelligence: 1}, new Weapon("Dagger", 1, 4), new Armor("Clothes", 0))
+    experience: 1000,
+    equipment: [new Weapon("Hammer", 2, 9)],
+    newHero: new Warrior("Fingolfin", GenderOptions.male, RaceOptions.elf, 1, 7, {attack: 3, sneak: 3, persuade: 1, intelligence: 1}, new Weapon("Sword", 2, 7), new Armor("Leather Armor", 5))
   },
-  nextChapter: Chapter2
+  nextChapter: null
 }
